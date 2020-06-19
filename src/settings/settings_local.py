96543853e246a283ae75bfa68f88779f5d40b635
KirillPanin <panin.kirill@gmail.com>
2020-06-19 06:14:47 -0700

    update

--- src/settings/settings_local.py
+++ src/settings/settings_local.py
@@ -1,16 +1,14 @@
 SECRET_KEY = '^ueo-4=t96eio5zjege^ae!0g15^tf2=yey5(360^^=jwl)m*d'
 
-DEBUG = True
-# False # python ./src/manage.py runserver --insecure
 CELERY_ALWAYS_EAGER = CELERY_TASK_ALWAYS_EAGER = True
 
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'devs.ops.tests@gmail.com'
+EMAIL_HOST_USER = 'dev.ops.tests@gmail.com'
 EMAIL_HOST_PASSWORD = 'brainfuck436'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
