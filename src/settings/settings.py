ba0b06731cbb3c648d8498fcd629a4f27fe4cbb9
KirillPanin <panin.kirill@gmail.com>
2020-06-19 16:48:33 -0700

    nginx+uwsgi

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -111,6 +111,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_content', 'static')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
